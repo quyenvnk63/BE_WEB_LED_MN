3638003164039545c8eab1468af24a608f77c430
quyen quyen <quyenvnk63@gmail.com>
2023-06-27 01:24:39 +0700

    fix error verceel

--- config/database.js
+++ config/database.js
@@ -1,22 +1,28 @@
+const mysql2 = require('mysql2');
 const { Sequelize } = require('sequelize');
-// import { Sequelize } from 'sequelize';
 require('dotenv').config();
 
 // Cấu hình kết nối cơ sở dữ liệu
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
-  });
+const options = {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql',
+  port: process.env.DB_PORT || 3306
+};
+
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
+}
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, options);
+
 // Kiểm tra kết nối cơ sở dữ liệu
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Kết nối cơ sở dữ liệu thành công' );
+    console.log('Kết nối cơ sở dữ liệu thành công');
   })
   .catch((error) => {
     console.error('Không thể kết nối cơ sở dữ liệu:', error);
   });
 
 module.exports = sequelize;
-// global.sequelize = sequelize;

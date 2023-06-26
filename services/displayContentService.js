445b14e5d93374b369ad45945b3139e4719f4dd7
quyen quyen <quyenvnk63@gmail.com>
2023-06-27 03:03:41 +0700

    update aws_bucket_name deploy_v5

--- services/displayContentService.js
+++ services/displayContentService.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
-const bucket  = process.env.BUCKET ;
+const bucket  = 'up-load-url';
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,

50fe4cfc870ae7359dbcec11583ec00b1c187347
quyen quyen <quyenvnk63@gmail.com>
2023-07-20 02:14:46 +0700

    fix redis serve-v2

--- config/redis.js
+++ config/redis.js
@@ -2,12 +2,11 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 require('dotenv').config();
-// console.log(process.env.REDIS_PASSWORD);
 // Create a Redis client
 const redisClient = redis.createClient({
-  host: "redis-17227.c17.us-east-1-4.ec2.cloud.redislabs.com" ,
-  port:17227 ,
-  password: "bJXnwCxAAtEQjntc54ownEqRlcHmTy1o" ,
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD ,
 });
 
 // Promisify Redis client methods for async/await
@@ -15,7 +14,7 @@ const getAsync = promisify(redisClient.get).bind(redisClient);
 const setAsync = promisify(redisClient.set).bind(redisClient);
 
 // Event listeners for Redis client
-// redisClient.connect();
+redisClient.connect();
 redisClient.on('error', (error) => {
   console.error('Error connecting to Redis:', error);
 });
